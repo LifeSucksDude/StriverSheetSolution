7756538e2511ff7d1d4138a8133f9d6742926ffc
LifeSucksDude <someshraj1595159@gmail.com>
2023-06-29 23:57:43 +0530

    solveditfinallywatchedsoln

--- RecursionRevision/combinationSum2.cpp
+++ RecursionRevision/combinationSum2.cpp
@@ -5,45 +5,36 @@ using namespace std;
 class Solution {
 public:
 void subSequence(int i,vector<int> &ds,vector<vector<int>> &ans,int target,int s,int n,vector<int> &candidates){
-        if(s>target){
+        if(target==0){
+            ans.push_back(ds);
             return;
         }
-        if(i==n){
-           
-            if(s==target){
-                ans.push_back(ds);
-                
+        for(int k=i; k<n; k++){
+            if(k>i && candidates[k]==candidates[k-1])continue; //avoid duplicate
+            if(candidates[k]>target){
+                break;
             }
-        return;
+          
+           
+            ds.push_back(candidates[k]);
+            
+            subSequence(k+1,ds,ans,target-candidates[k],s,n,candidates);
+            ds.pop_back();
+       
+          
         }
-        ds.push_back(candidates[i]);
-        s+=candidates[i];
-        subSequence(i+1,ds,ans,target,s,n,candidates);
-         ds.pop_back();
-        s=s-candidates[i];
-         subSequence(i+1,ds,ans,target,s,n,candidates);
-
       
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         //This is exactly the problem of the target sum and is pretty easy rn for me.
         //This is quiet diff as we want only unique and answer in sorted order.
-        //This way is giving tle.
-        sort(candidates.begin(),candidates.end());
+      sort(candidates.begin(),candidates.end());
         vector<int> ds;
         vector<vector<int>> ans;
         subSequence(0,ds,ans,target,0,candidates.size(),candidates);
-           map<vector<int>, int> uniqueVectors;
-            for (const vector<int>& vec : ans) {
-            uniqueVectors[vec] = 1;
+          
 
-        }
-        vector<vector<int>> result;
-        for (const auto& pair : uniqueVectors) {
-            result.push_back(pair.first);
-        }
-
-        return result;
+        return ans;
        
       
        
